30c7eb798741ab1114f379910a2755dcb12b674a
jasmeetsingh-1 <jasmeet.singh1@grazitti.com>
2024-09-08 11:26:04 +0530

    new experince

--- src/components/experinceData.jsx
+++ src/components/experinceData.jsx
@@ -41,14 +41,28 @@ const experienceData = [
   },
   {
     title: "Web Development Intern",
-    company: "Octopus tech solutions Pvt. Ltd.",
+    company: "Octopus Tech Solutions Pvt. Ltd.",
     location:"Mohali, Punjab",
     startDate: "Jan 2023",
     endDate: "May 2023",
     description: [
-      "Learned HTML5 and CSS3 while working as a Web Development Intern",
-      "Developed practical skills in web development through hands-on experience",
-      "Created an investment calculator project using HTML and CSS principles",
+      "Developed and integrated user interfaces to improve the functionality and user experience of web applications.",
+      "Collaborated with senior developers to refine code quality and implement best practices.",
+      "Worked on optimizing components for maximum performance across various web browsers and devices.",
+      "Contributed to the development of responsive web applications, ensuring a seamless experience for users.",
+      "Actively engaged in team meetings and discussions to provide input on project development and improvements.",
+    ],
+  },  
+  {
+    title: "Jr. Software Developer",
+    company: "Khannba Enterprises Pvt. Ltd.",
+    location:"Amritsar, Punjab",
+    startDate: "Sept 2020",
+    endDate: "July 2021",
+    description: [
+      "Responsible for developing in-house projects using ReactJS",
+      "Gained hands-on experience with cutting-edge web development technologies",
+      "Enhanced practical skills in web development through real-world applications",
     ],
   },
 ];
