997003cc6f1d321dafbc84f37940dce8644d8397
jasmeetsingh-1 <jasmeet.singh1@grazitti.com>
2025-09-21 19:39:33 +0530

    correct the pageNavigation

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import ShowTestimony from "./components/utils/showTestimony";
 
 
 function App() {
-  const [pageNavigate, setPageNavigate] = useState(false);
+  const [pageNavigate, setPageNavigate] = useState(true);
   return (
     <Routes>
       <Route path="/" element={
